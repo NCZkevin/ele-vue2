a80000b2b7946f6fb4c9a25ce5f5aa6fc59cdaa5
nczkevin <z1376871863@163.com>
2016-11-21 16:46:40 +0800

    goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,20 +1,50 @@
 <template>
-  <div>
-    <div class="ullist">
+  <div class="goods">
+    <div class="menu-wrapper">
 
     </div>
-    <div class="">
+    <div class="foods-wrapper">
 
     </div>
   </div>
 </template>
 
-<style>
-
-</style>
-
 <script>
     export default{
-
+      props: {
+        seller: {
+          type: Object
+        }
+      },
+      data() {
+        return {
+          goods: []
+        }
+      },
+      created() {
+        this.$http.get('/api/goods').then((response) => {
+          response = response.body
+          if (response.errno === 0) {
+            this.goods = response.data
+          }
+        })
+      }
     }
 </script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    width: 100%
+    bottom: 46px
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
+
+</style>
